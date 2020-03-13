4088843c0ddafe31663ca6a3a0abbad7759b7e25
권기훈 <inyourcare@nomadconnection.com>
2020-03-13 17:49:52 +0900

    문제이해

--- src/Fraudulent_Activity_Notifications.java
+++ src/Fraudulent_Activity_Notifications.java
@@ -11,23 +11,34 @@ public class Fraudulent_Activity_Notifications {
     // Complete the activityNotifications function below.
     static int activityNotifications(int[] expenditure, int d) {
 
+        if (expenditure.length <= d)
+            return 0;
+
         int[] trailingDaysExpenditure = new int[d];
-        int sum = 0;
         int notiCnt = 0;
         int trailIdx;
         for (int i = 0 ; i < d ; i++) {
             trailingDaysExpenditure[i] = expenditure[i];
-            sum += expenditure[i];
         }
-        double avg;
-        for (int i = d ; i<expenditure.length ; i++){
-            avg = (double) sum/d;
-            if (expenditure[i] >= avg*2)
-                notiCnt++;
-            trailIdx = i%d;
-            sum -= trailingDaysExpenditure[trailIdx];
-            trailingDaysExpenditure[trailIdx] = expenditure[i];
-            sum += trailingDaysExpenditure[trailIdx];
+
+        if (d % 2 == 0){
+            for (int i = d ; i<expenditure.length ; i++){
+                int[] medianArr = trailingDaysExpenditure.clone();
+                Arrays.sort(medianArr);
+                if (expenditure[i] >= (medianArr[d/2] + medianArr[d/2-1]))
+                    notiCnt++;
+                trailIdx = i%d;
+                trailingDaysExpenditure[trailIdx] = expenditure[i];
+            }
+        }else {
+            for (int i = d ; i<expenditure.length ; i++){
+                int[] medianArr = trailingDaysExpenditure.clone();
+                Arrays.sort(medianArr);
+                if (expenditure[i] >= medianArr[d/2]*2)
+                    notiCnt++;
+                trailIdx = i%d;
+                trailingDaysExpenditure[trailIdx] = expenditure[i];
+            }
         }
 
         return notiCnt;
